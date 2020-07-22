fbe1801088e8057631186999ca3f95e056d19ad3
S0f1eM <s.marchand56@gmail.com>
2020-07-22 18:57:47 +0200

    add details on results : coordinates and a bit of styling

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
 	baseURL: 'https://api.yelp.com/v3/businesses',
 	headers: {
-		Authorization: ''
+		Authorization: 'Bearer n-3dFpsY5WtszBP67iJiYLLqlmV9dxyC05vWR265DG7p9Iluy7qQo7KPI9adzAM4xVa3u23ds3r6jnIvnOnnCLTxthkHttT2cikYzxzzd7clmbacjsrKH84fUIwVX3Yx'
 	}
 });
 
